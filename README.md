19eee8d4eec00a54f6e5d857736abbd5e516cd96
smpr <gcbrown1116@gmail.com>
2017-09-11 22:53:15 -0400

    Got the Onclick to work and set up each spot with a unique id. then pulled their location using console log by breaking the string up as an array to display the col/row

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # project-1
+started by using the flex model to get the layout of the board going. broke it into a table inside of the center tile of a larger table. then used jquerry to select the tile and change the class to represent where the user was clicking
--- js/code.js
+++ js/code.js
@@ -1,6 +1,10 @@
 console.log("and we are up!")
-$().click(function(){ 
-    $(this).removeClass(".emptytile");
+$('.cleartile').click(function(){ 
+    //identifies the row the piece was clicked by breaking the id into an array and selecting the first part of the string
+    console.log('row is:',this.id[0]);
+    //indentifies the col the piece was clicked by breaking the id into an array and selecting the second part of the string
+    console.log('col is:',this.id[1]);
+    $(this).removeClass();
     $(this).addClass("playerRedTile");
     console.log("It worked");
  });
