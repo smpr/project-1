7d8a852f345ef305ae25805ac5c48ec2d0de119f
smpr <gcbrown1116@gmail.com>
2017-09-12 22:17:23 -0400

    Started setting up array tables and player name input. having problems getting it to load on the html

--- js/code.js
+++ js/code.js
@@ -1,4 +1,17 @@
-console.log("and we are up!")
+let player1Name = "Player 1";
+let player2Name = "Player 2";
+//gameboard array to store what piece is where using rcola
+let gameboardArray = [[]];
+//Prompt to ask and reassign player one and two names
+let player1NameAssign = function(){
+    player1Name = prompt("Player 1 Name?", "Player 1");
+    player1Name.replace("player1Name");
+    return player1Name;
+};
+player1NameAssign();
+$("player1NameDisplay").text(player1Name)
+
+//selector for tile
 $('.cleartile').click(function(){ 
     //identifies the row the piece was clicked by breaking the id into an array and selecting the first part of the string
     console.log('row is:',this.id[0]);
@@ -6,6 +19,12 @@ $('.cleartile').click(function(){
     console.log('col is:',this.id[1]);
     $(this).removeClass();
     $(this).addClass("playerRedTile");
+    //code to parse and push to the game board array
     console.log("It worked");
  });
+let currentPlayer = function(){
+    //this is where the color will switch and store the move to the player tracker and the winchecker array using if else to change the state
+
+};
+// 
  
