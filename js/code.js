e33cb15eeb68fda11f4a520aaacffe9cd38d5b3e
smpr <gcbrown1116@gmail.com>
2017-09-13 15:16:58 -0400

    Got the player swap to work(finally)

--- js/code.js
+++ js/code.js
@@ -1,10 +1,15 @@
+let player = "red";
+//player 1 vars
 let player1Name = "Player 1";
-let player2Name = "Player 2";
-//gameboard array to store what piece is where using rcola
 let redGameboardArray = [[]];
-let redMoveTracker = 0;
+let intRedMoveTracker = 0;
+let redMoveTracker = "";
+let test = "doesnt work";
+// player 2 vars
+let player2Name = "Player 2";
 let blackGameboardArray = [[]];
 let blackMoveTracker = 0;
+let intBlackMoveTracker = 0;
 //Prompt to ask and reassign player one and two names
 let player1NameAssign = function(){
     player1Name = prompt("Player 1 Name?", "Player 1");
@@ -18,31 +23,67 @@ let player2NameAssign = function(){
 };
 //this assigns the name value entered on initial promt to both the name and the moves tracker
 player1NameAssign();
-$(".player1NameDisplay").text(player1Name)
-$(".nameTablePlayer1MoveTracker").text(player1Name)
+$(".player1NameDisplay").text(player1Name);
+$(".nameTablePlayer1MoveTracker").text(player1Name);
+$(".movesTablePlayer1MoveTracker").html(intRedMoveTracker);
 player2NameAssign();
 $(".player2NameDisplay").text(player2Name)
 $(".nameTablePlayer2MoveTracker").text(player2Name)
-//selector for red tile
-let redPlayerSelector = $('.cleartile').click(function(){ 
-    //identifies the row the piece was clicked by breaking the id into an array and selecting the first part of the string
-    let rowRef = this.id[0];
-    intRowRef = Number(rowRef);
-    console.log('row is:',this.id[0]);
-    //indentifies the col the piece was clicked by breaking the id into an array and selecting the second part of the string
-    console.log('col is:',this.id[1]);
-    let colRef = this.id[1];
-    intColRef = Number(colRef)
-    gameboardArray = [intRowRef,intColRef];
-    $(this).removeClass();
-    $(this).addClass("playerRedTile");
-    //code to parse and push to the game board array
-    console.log("It worked");
-    return redGameboardArray;
- });
-let currentPlayer = function(){
-    //this is where the color will switch and store the move to the player tracker and the winchecker array using if else to change the state
 
+
+let currentPlayer = function(){
+    if (player === "red"){
+            //identifies the row the piece was clicked by breaking the id into an array and selecting the first part of the string
+            let rowRef = this.id[0];
+            //intRowRef = Number(rowRef);
+            console.log(' Red row is:',this.id[0]);
+            //indentifies the col the piece was clicked by breaking the id into an array and selecting the second part of the string
+            console.log(' Red col is:',this.id[1]);
+            let colRef = this.id[1];
+            //intColRef = Number(colRef)
+            //redGameboardArray = [intRowRef,intColRef];
+            $(this).removeClass();
+            $(this).addClass("playerRedTile");
+            intRedMoveTracker = intRedMoveTracker + 1;
+            console.log("Red Turn Complete");
+            // return redGameboardArray;
+            //updates the ammount of moves red player has made
+            // return intRedMoveTracker;
+           //redMoveTracker = intRedMoveTracker.toString();
+           // return redMoveTracker;
+           player = "black";
+          test = "works";
+          return test;
+           //currentPlayer();
+           
+         
+            
+    } 
+    else {
+        
+            //identifies the row the piece was clicked by breaking the id into an array and selecting the first part of the string
+            let rowRef = this.id[0];
+            intRowRef = Number(rowRef);
+            console.log('row is:',this.id[0]);
+            //indentifies the col the piece was clicked by breaking the id into an array and selecting the second part of the string
+            console.log('col is:',this.id[1]);
+            let colRef = this.id[1];
+            intColRef = Number(colRef)
+         blackGameboardArray = [intRowRef,intColRef];
+            $(this).removeClass();
+            $(this).addClass("playerBlackTile");
+            intBlackMoveTracker = intBlackMoveTracker + 1;
+            console.log("Black Turn Complete");
+            //updates the ammount of moves black player has made
+            player = "red";
+            return player;
+
+        
+    }
+    //this is where the color will switch and store the move to the player tracker and the winchecker array using if else to change the state
+    
+   return player; 
 };
-// 
+$('.cleartile').on("click",currentPlayer);
+//debugger
  
