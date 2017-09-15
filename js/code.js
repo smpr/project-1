0aad81e3f5381bc9fe05542111e3a25baf4e0b78
smpr <gcbrown1116@gmail.com>
2017-09-15 08:30:23 -0400

    properly setup the 2d array. and added a extra column to the game goard

--- js/code.js
+++ js/code.js
@@ -14,7 +14,7 @@ let intBlackMoveTracker = 0;
 let player1NameAssign = function(){
     player1Name = prompt("Player 1 Name?", "Player 1");
     player1Name.replace("player1Name");
-    return player1Name;
+    
 };
 let player2NameAssign = function(){
     player2Name = prompt("Player 2 Name?", "Player 2");
@@ -28,17 +28,6 @@ let openSpace = function(){
 };
 //this will check for a winner up or down
 
-let verticalWin = function(){
-//needs to use .length so it cycles through the entire array
-
-for(let i = 0; i < gameLocationArray.length; i++) {
-        
-        //console.log(redVert[j])
-        //console.log("redVert[" + i + "][" + j + "] = " + redGameboardArray[j]);
-    
-}
-
-};
 //this assigns the name value entered on initial promt to both the name and the moves tracker
 player1NameAssign();
 $(".player1NameDisplay").text(player1Name);
@@ -47,7 +36,17 @@ player2NameAssign();
 $(".player2NameDisplay").text(player2Name)
 $(".nameTablePlayer2MoveTracker").text(player2Name)
 
-
+let setupGameboard = function(){
+    gameLocationArray=[[]];
+    for(i=0;i<7;i++){
+        gameLocationArray[i] = [];
+        for(j=0;j<7;j++){
+            gameLocationArray[i][j]= "unassigned";
+            
+        };
+    };
+};
+setupGameboard();
 let currentPlayer = function(){
     if (player === "red"){
             //identifies the row the piece was clicked by breaking the id into an array and selecting the first part of the string
@@ -59,13 +58,13 @@ let currentPlayer = function(){
             //indentifies the col the piece was clicked by breaking the id into an array and selecting the second part of the string
             console.log(' Red col is:',this.id[1]);
             let intColRef = Number(colRef)
-            let thisCord = [intRowRef, intColRef]
-            gameLocationArray.push(player,thisCord);
+            //let thisCord = [intRowRef, intColRef]
+            gameLocationArray[intRowRef][intColRef]=player;
             //let innerRedGameboardArray = [intRowRef,intColRef]
             //redGameboardArray.push(innerRedGameboardArray)
             //needs to be deleted after games array is fixedredGameboardArray = redGameboardArray + [intRowRef,intColRef];
             $(this).removeClass();
-            $(this).addClass("playerRedTile");
+            $(this ).addClass("playerRedTile");
 
             intRedMoveTracker = intRedMoveTracker + 1;
             console.log("Red Turn Complete");
@@ -73,7 +72,7 @@ let currentPlayer = function(){
            //redMoveTracker = intRedMoveTracker.toString();
            player = "black";
            $(".movesTablePlayer1MoveTracker").html(intRedMoveTracker);
-           verticalWin();
+           console.log(gameLocationArray)
           
    } 
     else {
@@ -87,10 +86,7 @@ let currentPlayer = function(){
          console.log(' Red col is:',this.id[1]);
          let intColRef = Number(colRef)
          let thisCord = [intRowRef, intColRef]
-         gameLocationArray.push(player,thisCord);
-         //let innerRedGameboardArray = [intRowRef,intColRef]
-         //redGameboardArray.push(innerRedGameboardArray)
-         //needs to be deleted after games array is fixedredGameboardArray = redGameboardArray + [intRowRef,intColRef];
+         gameLocationArray[intRowRef][intColRef]=player;        
          $(this).removeClass();
          $(this).addClass("playerBlackTile");
 
@@ -100,26 +96,8 @@ let currentPlayer = function(){
         //redMoveTracker = intRedMoveTracker.toString();
         player = "red";
         $(".movesTablePlayer2MoveTracker").html(intBlackMoveTracker);
-        verticalWin();
-            ////identifies the row the piece was clicked by breaking the id into an array and selecting the first part of the string
-            //let rowRef = this.id[0];
-            //intRowRef = Number(rowRef);
-            //console.log('row is:',this.id[0]);
-            ////indentifies the col the piece was clicked by breaking the id into an array and selecting the second part of the string
-            //console.log('col is:',this.id[1]);
-            //let colRef = this.id[1];
-            //intColRef = Number(colRef)
-            //let innerBlackGameboardArray = [intRowRef,intColRef]
-            //blackGameboardArray.push(innerBlackGameboardArray)
-            //$(this).removeClass();
-            //$(this).addClass("playerBlackTile");
-            //intBlackMoveTracker = intBlackMoveTracker + 1;
-            //console.log("Black Turn Complete");
-            ////updates the ammount of moves black player has made
-            //player = "red";
-            //$(".movesTablePlayer2MoveTracker").html(intBlackMoveTracker);
-            //return player;
 
+        console.log(gameLocationArray)
         
     }
     //this is where the color will switch and store the move to the player tracker and the winchecker array using if else to change the state
